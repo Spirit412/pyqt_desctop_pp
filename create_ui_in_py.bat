507c6ad34bfce44f5b9c5ea76e1c3e3b180ee148
Aleksandr Dom <spirit412@gmail.com>
2023-02-10 17:04:04 +0300

    Start

---
+++ create_ui_in_py.bat
@@ -0,0 +1,3 @@
+@REM pyuic6 interface.ui -o interface.py
+pyuic6 interface.ui -o interface.py
+pyrcc6 resources.qrc -o resources_rc.py
---
+++ main.py
@@ -0,0 +1,30 @@
+from ui_interface import *
+import os
+import sys
+
+
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt6 import QtCore
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        QtCore.QDir.addSearchPath('icon', '/icons')
+        
+        self.show()
+    
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+    
+# app = QApplication([])
+
+# window = QWidget()
+
+# window.show()
+
+# app.exec()
